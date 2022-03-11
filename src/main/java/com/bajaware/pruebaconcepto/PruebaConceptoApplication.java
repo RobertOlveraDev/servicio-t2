c098eb32dfb0ff50b50a3f810a030c5b853364e9
RobertOlveraDev <47266813+RobertOlveraDev@users.noreply.github.com>
2022-03-10 23:28:27 -0600

    Update PruebaConceptoApplication.java

--- src/main/java/com/bajaware/pruebaconcepto/PruebaConceptoApplication.java
+++ src/main/java/com/bajaware/pruebaconcepto/PruebaConceptoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
+@RequestMapping(value = "/app")
 public class PruebaConceptoApplication {
 
 	@GetMapping("/")
