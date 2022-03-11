5ad83f72739b704e3f6f408febf170b9fd49186e
Roberto <jdeveloper@bajaware.com>
2022-03-10 22:48:11 -0600

    Initial Commit

---
+++ src/main/java/com/bajaware/pruebaconcepto/PruebaConceptoApplication.java
@@ -0,0 +1,25 @@
+package com.bajaware.pruebaconcepto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class PruebaConceptoApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "Prueba de concepto servicio 2";
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Prueba hello servicio 2";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaConceptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajaware/pruebaconcepto/PruebaConceptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaware.pruebaconcepto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaConceptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
